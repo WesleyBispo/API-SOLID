1f6bd5188e4ee016c750ca0b01b6521a4ce9d8c6
WesleyBispo <wesley17bispo@gmail.com>
2025-04-11 07:21:38 -0300

    feat add new router to gyms flow

---
+++ src/http/controllers/users/routes.ts
@@ -0,0 +1,19 @@
+import { FastifyInstance } from 'fastify'
+import { register } from './register'
+import { authenticate } from './authenticate'
+import { profile } from './profile'
+import { verifyJWT } from '@/http/middlewares/verify-jwt'
+
+export async function usersRoutes(app: FastifyInstance) {
+  app.post('/users', register)
+  app.post('/sessions', authenticate)
+
+  // Authenticate Routes
+  app.get(
+    '/me',
+    {
+      onRequest: [verifyJWT],
+    },
+    profile,
+  )
+}
