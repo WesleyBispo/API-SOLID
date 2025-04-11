6b873f9e233568a9e97d69a823e0dee9e06364b5
WesleyBispo <wesley17bispo@gmail.com>
2025-04-10 22:16:01 -0300

    enviroment e2e test

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,9 +1,27 @@
+import 'dotenv/config'
+
+import { randomUUID } from 'node:crypto'
+import { execSync } from 'node:child_process'
 import type { Environment } from 'vitest/environments'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+function generateDatabaseURL(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Please provide a DATABASE_URL environment variable')
+  }
+
+  const url = new URL(process.env.DATABASE_URL)
+
+  url.searchParams.set('schema', schema)
+
+  return url.toString()
+}
 
 export default <Environment>{
-  name: 'custom',
+  name: 'prisma',
   transformMode: 'ssr',
-  // optional - only if you support "experimental-vm" pool
   async setupVM() {
     const vm = await import('node:vm')
     const context = vm.createContext()
@@ -11,18 +29,24 @@ export default <Environment>{
       getVmContext() {
         return context
       },
-      teardown() {
-        // called after all tests with this env have been run
-      },
+      teardown() {},
     }
   },
   async setup() {
-    console.log('Setup environment')
-    // custom setup
+    const schema = randomUUID()
+    const databaseURL = generateDatabaseURL(schema)
+
+    process.env.DATABASE_URL = databaseURL
+
+    execSync('npx prisma migrate deploy')
+
     return {
       async teardown() {
-        console.log('Teardown environment')
-        // called after all tests with this env have been run
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE;`,
+        )
+
+        await prisma.$disconnect()
       },
     }
   },
